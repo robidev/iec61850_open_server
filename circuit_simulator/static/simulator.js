d843b16eaa58fdedd7dec567a16c93d5671973dd
Robin M <robin.dev@gmail.com>
2020-07-02 19:17:33 +0200

    ui update

--- circuit_simulator/static/simulator.js
+++ circuit_simulator/static/simulator.js
@@ -8,13 +8,14 @@ $(document).ready(function() {
 			url: url, 
 			success: function(data){
 				var c = $('div#container')[0];
-				var d = JSON.stringify(data).replace(/{|}|\"/g,"");
-				c.innerHTML= d;  
+				var d = JSON.stringify(data).replace(/{|}|\"/g,"").replace(/\\n/g,"<br>");
+				c.innerHTML= "<pre style='white-space: pre-line'>" + d + "</pre>";  
 				//update png
 				var timestamp = new Date().getTime();  
 				var el = document.getElementById("plot");  
 				var queryString = "?t=" + timestamp;    
-				el.src = "static/plot.png" + queryString;                     
+				el.src = "static/plot.png" + queryString;  
+				update_settings();                   
 			}
 			});
 		}
@@ -23,15 +24,23 @@ $(document).ready(function() {
 			if(this.id == "enable_settings"){
 				console.log("enable_settings")
 				$('#simulation_settings').toggle();
+				$('#simulation_node').hide();
 			}
 			if(this.id == "modify_nodes"){
 				console.log("modify_nodes");
 				$('#simulation_node').toggle();
+				$('#simulation_settings').hide();
 			}
 		}
+		update_settings();
 	    e.preventDefault(); // stop the browser from following the link
 	});
 
+	update_settings();
+})
+
+function update_settings()
+{
 	$.ajax({
 		type: "get",
 		url: "/simulation_settings", 
@@ -41,5 +50,4 @@ $(document).ready(function() {
 			$('input#tran')[0].value = data["tran"];            
 		}
 	});
-})
-
+}
