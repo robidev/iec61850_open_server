2490ca813b364f6dc24bdc09440b1d2d96494fdb
Robin Massink <robin.massink@alliander.com>
2024-03-14 09:26:40 +0100

    added callback python extref decoding

--- plugin/python_app/app.py
+++ plugin/python_app/app.py
@@ -5,9 +5,11 @@ import python_plugin
 # functions for working with MmsValue's and such
 import lib61850
 
+
 # note, the extref parameter contains a callback parameter, but it is in a c-struct.
 def cb(extref):
-    print("callback")
+    var = ctypes.cast(extref,ctypes.POINTER(python_plugin.InputEntry)).contents # cast the extref struct
+    print("PYTHON_APP: callback for '" + var.Ref + "' with argument: " + var.callBackParam)
 
 def main(server_instance):
     # convert the server instance to the right type
@@ -18,14 +20,14 @@ def main(server_instance):
 
     # check if the MmsValue* could be found
     if not mms_pos:
-        print("could not find mms_pos data ref @IED1_XCBRGenericIO/XCBR1.Pos.stVal. quitting...")
+        print("PYTHON_APP: could not find mms_pos data ref @IED1_XCBRGenericIO/XCBR1.Pos.stVal. quitting...")
         return 1
 
-    print("found mms_pos data ref @IED1_XCBRGenericIO/XCBR1.Pos.stVal")
+    print("PYTHON_APP: found mms_pos data ref @IED1_XCBRGenericIO/XCBR1.Pos.stVal")
 
     # register a callback for when a DA is updated
     cbRef = python_plugin.callBackFunction(cb)
-    python_plugin.registerDaCallback(srv,"IED1_XCBRGenericIO/XCBR1.Pos.stVal",cbRef,None)
+    python_plugin.registerDaCallback(srv,"IED1_XCBRGenericIO/XCBR1.Pos.stVal",cbRef,42)
 
     # cast the type to the correct MmsValue* type for use by libiec61850
     pos = ctypes.cast(mms_pos,ctypes.POINTER(lib61850.struct_sMmsValue ) )
@@ -34,14 +36,14 @@ def main(server_instance):
     # Use libiec61850 C documentation to see what functions exist. 
     # Ensure base types(int, bool, float) or MmsValue* is the input/output. 
     # Else you need to cast/convert using ctype library functions (see iec61850_open_client for more complex examples)
-    print("position value: ", str(lib61850.MmsValue_getBitStringAsInteger(pos)) )
+    print("PYTHON_APP: position value: ", str(lib61850.MmsValue_getBitStringAsInteger(pos)) )
 
     # the python plugin can update most basic types, such as bool, bitstring and VisString
     # this will also update GOOSE, SMV and notify extref connected logical nodes if connected via input definitions
     python_plugin.updateDataRefBitString(srv,"IED1_XCBRGenericIO/XCBR1.Pos.stVal",2)
 
     # read out the new value. the MmsValue* is a pointer, so it will contain the updated value.
-    print("new position value: ", str(lib61850.MmsValue_getBitStringAsInteger(pos)) )
+    print("PYTHON_APP: new position value: ", str(lib61850.MmsValue_getBitStringAsInteger(pos)) )
 
     return 0
 
